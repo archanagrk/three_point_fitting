843440cbf3dd6da752d98cb85eda21a60e74926b
Archana Radhakrishnan <>
2019-07-17 15:28:57 -0400

    Tuning the fit quality

--- build/build_mac.sh
+++ build/build_mac.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-../configure --prefix="${HOME}/three_pt_analysis/three_pt_fit_install" \
-             --with-adat="${HOME}/Scattering/adat_install" \
-              --with-itpp="${HOME}/Scattering/itpp_install" \
-              --with-fitting="${HOME}/three_pt_analysis/fitting_lib_install" \
-              --with-semble="${HOME}/three_pt_analysis/semble_install" \
+../configure --prefix="${HOME}/LQCDSoftware/three_pt_analysis/three_pt_fit_install" \
+             --with-adat="${HOME}/LQCDSoftware/Kinematic_factor/adat_devel_install" \
+              --with-itpp="${HOME}/LQCDSoftware/Scattering/itpp_install" \
+              --with-fitting="${HOME}/LQCDSoftware/three_pt_analysis/fitting_lib_install" \
+              --with-semble="${HOME}/LQCDSoftware/three_pt_analysis/semble_install" \
              #CXXFLAGS="-Wall -g -O3 -mtune=native -fopenmp -fbounds-check" CFLAGS="-Wall -g -O3 -mtune=native -fopenmp -fbounds-check" \
              CXXFLAGS="-Wall -O3 -mtune=native -fopenmp -fbounds-check" CFLAGS="-Wall -O3 -mtune=native -fopenmp -fbounds-check"\
-             CXX=g++-8 CC=gcc-8
+             CXX=g++-9 CC=gcc-9
              #--with-gcc-8="/usr/local/Cellar/gcc/8.2.0/"
